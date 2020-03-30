c8531755aa5d3311be6041434afee1906a473eeb
Caio Saad Mantovani <c.saadmantovani@gmail.com>
2020-03-30 16:54:07 -0300

    entrega dos exercícios feitos ate o momento

---
+++ semana3/logicabooleana/js-vanilla-template/src/index.js
@@ -0,0 +1,50 @@
+/*
+a - false
+b - false
+c - true
+d - false
+e - boolean
+
+I - undefined
+II - null
+III - 11
+IV - 3 e 4
+V - 19 e 9
+VI - 3
+VII - 1
+
+
+a - array é uma variável em que se pode declarar multiplos tipos de valores.
+
+b - 0
+
+c - array.length
+
+
+
+*/
+
+
+const valorF = 77
+const valorK = (valorF - 32)*5/9 + 273
+const result1 = "a - " + valorK + " K"
+console.log(result1)
+
+const valorC = 80
+const valorF2 = (valorC)*9/5 + 32
+const result2 = "b - " + valorF2 + " ºF"
+console.log(result2)
+
+const valorC2 = 30
+const valorF3 = (valorC2)*9/5 + 32
+const valorK2 = valorC2 + 273
+const result3 = "c - " + valorF3 + " ºF " + valorK2 + " K"
+console.log(result3)
+
+let valorC3 = 30
+const valorF4 = (valorC3)*9/5 + 32
+const valorK3 = valorC3 + 273
+const result4 = "c - " + valorF4 + " ºF " + valorK3 + " K"
+console.log(result4)
+
+// trabalhando nos exercícios seguintes
