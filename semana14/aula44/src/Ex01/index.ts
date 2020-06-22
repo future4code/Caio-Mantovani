4d36b439d74274830864f1652d8bdeb85087d5d6
Caio Mantovani <c.saadmantovani@gmail.com>
2020-06-22 16:51:55 -0300

    entrega

---
+++ semana14/aula44/src/Ex01/index.ts
@@ -0,0 +1,37 @@
+//a
+const minhaString: string = "Labenu"
+//b
+const meuNumero: number = 5 //podemos utilizar string |number
+//c & d
+type pessoa = {
+    nome: string,
+    idade: number,
+    corFavorita: string
+}
+const alice: pessoa = {
+    nome: "Alice",
+    idade: 18,
+    corFavorita: "vermelho"
+}
+const bob: pessoa = {
+    nome: "bob",
+    idade: 20,
+    corFavorita: "branco"
+}
+const bananinha: pessoa = {
+    nome: "bananinha",
+    idade: 22,
+    corFavorita: "amarelo"
+}
+
+//e
+
+enum cores {
+    VIOLETA = "Violeta",
+    ANIL = "Anil",
+    AZUL = "Azul",
+    VERDE = "Verde",
+    AMARELO = "Amarelo",
+    LARANJA = "Laranja",
+    VERMELHO = "Vermelho"
+}
---
+++ semana14/aula44/src/Ex04/index.ts
@@ -0,0 +1,31 @@
+type post = {
+    autor: string,
+    texto: string
+}
+const postagemA: post = {
+    autor: "autorA",
+    texto: "textoA"
+}
+
+const postagemB: post = {
+    autor: "autorB",
+    texto: "textoB"
+}
+
+const postagemC: post = {
+    autor: "autorC",
+    texto: "textoC"
+}
+
+const postagemD: post = {
+    autor: "autorD",
+    texto: "textoD"
+}
+
+const postagem01: post = {
+    autor: "autorE",
+    texto: "textoE"
+}
+
+const listaPosts: postagens[] = 
+// terminar função
